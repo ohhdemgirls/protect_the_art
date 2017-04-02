c45c24c5b399410273eededbe120122da3d67a4e
Erik Nordstrøm <erik@nordstroem.no>
2017-04-02 16:08:42 +0200

    Change config values.

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-canvas_refresh_interval_seconds = 30
-scanband_nominal_height_px = 5
+canvas_refresh_interval_seconds = 5
+scanband_nominal_height_px = 4
 ua = 'Protect the Art/0.2.0; +https://github.com/eriknstr/protect_the_art'
